94bf7c89e6a0c66a796a4d12cbc453eed1c80333
Larry Saytee <saytee.software@web-developer.one>
2025-07-01 19:40:50 +0000

    make "Swap", "Limit", "Buy", "Sell" the same "Tokens", "Pools", "Transac

--- src/components/trade-nav.tsx
+++ src/components/trade-nav.tsx
@@ -7,21 +7,23 @@ import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export function TradeNav() {
   const pathname = usePathname();
+  const activeClass = "data-[state=active]:bg-primary/10 data-[state=active]:text-primary dark:data-[state=active]:bg-background dark:data-[state=active]:text-foreground";
+
 
   return (
     <div className="w-full max-w-md">
         <Tabs value={pathname} className="w-full">
             <TabsList className="grid w-full grid-cols-4">
-                <TabsTrigger value="/" asChild>
+                <TabsTrigger value="/" asChild className={activeClass}>
                     <Link href="/">Swap</Link>
                 </TabsTrigger>
-                <TabsTrigger value="/limit" asChild>
+                <TabsTrigger value="/limit" asChild className={activeClass}>
                     <Link href="/limit">Limit</Link>
                 </TabsTrigger>
-                <TabsTrigger value="/buy" asChild>
+                <TabsTrigger value="/buy" asChild className={activeClass}>
                     <Link href="/buy">Buy</Link>
                 </TabsTrigger>
-                <TabsTrigger value="/sell" asChild>
+                <TabsTrigger value="/sell" asChild className={activeClass}>
                     <Link href="/sell">Sell</Link>
                 </TabsTrigger>
             </TabsList>
