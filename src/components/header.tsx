03cb55c62320cc4959dc9522afcde030b87b0822
Larry Saytee <saytee.software@web-developer.one>
2025-07-01 20:08:20 +0000

    change theme to be just like the theme in https://cryptodx-v3.netlify.ap

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -95,7 +95,7 @@ export function Header({ cryptocurrencies }: { cryptocurrencies: Cryptocurrency[
 
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-card text-card-foreground border-border">
+    <header className="sticky top-0 z-50 w-full border-b bg-background text-card-foreground border-border">
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-3">
