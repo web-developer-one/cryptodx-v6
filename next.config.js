d406e07fa41774b31988c14da9f78cffec7da529
Larry Saytee <saytee.software@web-developer.one>
2025-07-06 00:33:51 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   devIndicators: {
     allowedDevOrigins: [
@@ -45,4 +43,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
