a1625314005aee7045cdcd01024bd53c78893fe2
Larry Saytee <saytee.software@web-developer.one>
2025-07-08 11:06:42 +0000

    DO NOT BREAK THE SITE > FIX > "./src/ai/genkit.ts:19:7 Type error: Objec

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -12,11 +12,6 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      // The Gemini 1.5 Flash model is a good balance of cost, speed, and quality.
-      // Other models can be used here as well.
-      // https://ai.google.dev/models/gemini
-      // gemini-1.5-flash-latest is an auto-updating version
-      model: 'gemini-1.5-flash-latest',
       // The embedding model is used for grounding and other tasks.
       embeddingModel: 'text-embedding-004',
     }),
