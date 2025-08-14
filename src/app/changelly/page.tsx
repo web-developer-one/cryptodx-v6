5324718d075f632f610eb264d852e519c61c6ddb
Larry Saytee <saytee.software@web-developer.one>
2025-08-14 16:42:06 +0000

    on the "changelly" page > make the Exchange Crypto box fit the height

--- src/app/changelly/page.tsx
+++ src/app/changelly/page.tsx
@@ -16,15 +16,17 @@ export default function ChangellyPage() {
 
   return (
     <div className="container flex-1 flex flex-col items-center py-8 gap-6">
-      <TradeNav />
-      <div className="w-full max-w-lg h-[390px]">
+      <div className="w-full max-w-lg flex justify-center">
+        <TradeNav />
+      </div>
+      <div className="w-full max-w-lg flex-1">
           <iframe
               src={widgetUrl}
               width="100%"
-              height="390px"
-              frameBorder="none"
+              height="100%"
+              frameBorder="0"
               allow="camera"
-          />
+          >Cant load widget</iframe>
       </div>
     </div>
   );
