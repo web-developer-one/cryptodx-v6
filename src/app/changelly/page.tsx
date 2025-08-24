34e4bef17509adb440f18e7a40083b3ce62013b9
Larry Saytee <saytee.software@web-developer.one>
2025-08-24 20:04:28 +0000

    change the "changelly" page to "changellyswap". do not break the site.

--- src/app/changelly/page.tsx
+++ src/app/changelly/page.tsx
@@ -1,33 +1,12 @@
-
 'use client';
 
-import { TradeNav } from "@/components/trade-nav";
-import { useEffect } from "react";
-import { useLanguage } from "@/hooks/use-language";
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 
-export default function ChangellyPage() {
-  const { t } = useLanguage();
-  
+export default function OldChangellyPage() {
   useEffect(() => {
-    document.title = t('PageTitles.changelly');
-  }, [t]);
+    redirect('/changellyswap');
+  }, []);
 
-  const widgetUrl = "https://widget.changelly.com?from=*&to=*&amount=0.1&address=&fromDefault=btc&toDefault=eth&merchant_id=9ZJKwWl1A23lMd36&payment_id=&v=3";
-
-  return (
-    <div className="container flex-1 flex flex-col items-center py-8 gap-6">
-      <div className="w-full max-w-lg flex justify-center">
-        <TradeNav />
-      </div>
-      <div className="w-full max-w-lg flex-1">
-          <iframe
-              src={widgetUrl}
-              width="100%"
-              height="600px"
-              frameBorder="0"
-              allow="camera"
-          >Cant load widget</iframe>
-      </div>
-    </div>
-  );
+  return null;
 }
--- src/components/trade-nav.tsx
+++ src/components/trade-nav.tsx
@@ -11,9 +11,9 @@ export function TradeNav() {
   const { t } = useLanguage();
 
   return (
-    <div className="w-full max-w-md">
+    <div className="w-full max-w-lg">
         <Tabs value={pathname} className="w-full">
-            <TabsList className="grid w-full grid-cols-4">
+            <TabsList className="grid w-full grid-cols-5">
                 <TabsTrigger value="/" asChild>
                     <Link href="/">{t('TradeNav.swap')}</Link>
                 </TabsTrigger>
@@ -25,6 +25,9 @@ export function TradeNav() {
                 </TabsTrigger>
                 <TabsTrigger value="/sell" asChild>
                     <Link href="/sell">{t('TradeNav.sell')}</Link>
+                </TabsTrigger>
+                <TabsTrigger value="/changellyswap" asChild>
+                    <Link href="/changellyswap">{t('TradeNav.changelly')}</Link>
                 </TabsTrigger>
             </TabsList>
         </Tabs>
