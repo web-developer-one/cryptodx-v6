05040783d4d9a98b772d746b1248d938607e1d25
Larry Saytee <saytee.software@web-developer.one>
2025-08-24 20:18:36 +0000

    do not ever add another item to the "Swap", "Limit", "Buy", "Sell" tab m

--- src/components/trade-nav.tsx
+++ src/components/trade-nav.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Link from 'next/link';
@@ -13,7 +12,7 @@ export function TradeNav() {
   return (
     <div className="w-full max-w-lg">
         <Tabs value={pathname} className="w-full">
-            <TabsList className="grid w-full grid-cols-5">
+            <TabsList className="grid w-full grid-cols-4">
                 <TabsTrigger value="/" asChild>
                     <Link href="/">{t('TradeNav.swap')}</Link>
                 </TabsTrigger>
@@ -25,9 +24,6 @@ export function TradeNav() {
                 </TabsTrigger>
                 <TabsTrigger value="/sell" asChild>
                     <Link href="/sell">{t('TradeNav.sell')}</Link>
-                </TabsTrigger>
-                <TabsTrigger value="/changellyswap" asChild>
-                    <Link href="/changellyswap">{t('TradeNav.changelly')}</Link>
                 </TabsTrigger>
             </TabsList>
         </Tabs>
