17d0b46d25696d17b4284ba34752f8cc24df0ce3
Larry Saytee <saytee.software@web-developer.one>
2025-08-29 19:06:43 +0000

    Diagnosis: The build failure is due to a missing module during the build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['crypto-js', '@paypal/checkout-server-sdk'],
+  },
   devIndicators: {
     allowedDevOrigins: [
       '*.cloudworkstations.dev',
