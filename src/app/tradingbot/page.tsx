948c7483f8c59e7623f74917003ad7561ac4af36
Larry Saytee <saytee.software@web-developer.one>
2025-08-29 23:47:59 +0000

    clean the site or app for deployment to Netlify.com

--- src/app/tradingbot/page.tsx
+++
@@ -1,8 +0,0 @@
-
-import { getLatestListings } from "@/lib/coinmarketcap";
-import { ArbitragePageClient } from "@/components/arbitrage-page-client";
-
-export default async function TradingBotPage() {
-    const { data: cryptoData, error } = await getLatestListings();
-    return <ArbitragePageClient cryptoData={error ? [] : cryptoData} error={error} />;
-}
